924037223eb429b0682aa57ebfa8d940d02085d3
Michael <michaeln870@gmail.com>
2021-03-23 16:06:54 -0400

    Updating app and codes

--- App/app.R
+++ App/app.R
@@ -1,35 +1,99 @@
 library(shiny)
+library(DT)
+
 source("functions.R")
 
 
 ui <- fluidPage(
-    titlePanel("Word Prediction Application"),
-    h3(strong("**Work in Progress**")),
-    textInput("string", "Write something:"),
-    textOutput("prediction"),
-    verbatimTextOutput("text"),
-    hr(),
-    HTML(
-      paste(
-        h5("This is only a prototype, the current algorithm is very simple and 
-           doesn't do a very good job at predicting the next word."),
-        h5("I still need to:"),
-        h5("- Implement a proper scoring algorithm"),
-        h5("- Build a system to display more suggestions"),
-        h5("- Test and optimize"),
-        h5("- Improve UI and add features")
-      )
+  tabsetPanel(
+    tabPanel("Word Predictor",
+      titlePanel("Next Word Prediction Application"),
+      sidebarLayout(
+        sidebarPanel(
+          textInput("string", "Please write something:"),
+          h5("Prediction:"),
+          htmlOutput("prediction"),
+          hr(),
+          sliderInput("slider", "Number of suggestions to be displayed",
+                      min=0, max= 30, value = 10),
+          checkboxInput("recursion", "Add recursion", value = FALSE),
+          checkboxInput("DT", "Add a searchable table", value = FALSE),
+          width = 6
+        ),
+        mainPanel(
+          h3("More Suggestions"),
+          tags$style(type="text/css",
+                     ".shiny-output-error { visibility: hidden; }",
+                     ".shiny-output-error:before { visibility: hidden; }"),
+          column(width=2, tableOutput('scores')),
+          column(width=2, tableOutput('scores2'), offset = 1),
+          column(width=2, tableOutput('scores3'), offset = 1),
+          width = 6
+        )
+      ),
+      conditionalPanel(
+        condition = "input.DT == true",
+        DTOutput("table"))
+    ),
+    tabPanel("About",
+             HTML("<h3>How it works:</h3>
+                <ul>
+                    <li>Use the text box to write something and look at the prediction under</li>
+                    <li>Look at the table on the right to see more suggestions</li>
+                    <li>A higher score means that the word is a better match according to the model, a score of 1 means a perfect match</li>
+                </ul>
+                <h3>Options:</h3>
+                <ul>
+                    <li><strong>Slider</strong>: Changes the number of suggestions displayed on the right (if number of suggestions doesn't change, it means that there are no more suggestions)</li>
+                    <li><strong>Add recursion</strong>: Look into lower order n-grams to give more suggestions</li>
+                    <li><strong>Add a searchable table</strong>: Adds a searchable table below with all available suggestions</li>
+                </ul>
+                <h3>About the model:</h3>
+                <p>The model has been built using a large corpus of more than 4 million lines of text with a total word count of over 102 million. The corpus has been cleaned and transformed into n-grams up to the fifth order (5-grams). The resulting model is composed of a corpus of more than 15 million n-grams, not accounting for unigrams. The algorithm used to predict the next word is based on the &quot;Stupid Backoff&quot; method.</p>
+                <h3>Potential improvements:</h3>
+                <p>The goal of this project was simply to demonstrate the ability to build a next word prediction application, improving it further is beyond its scope. Nonetheless, it is still useful to recognized its flaws and what steps could be taken to improve the model. Here are a few ideas:</p>
+                <ul>
+                    <li><strong>Use a better prediction method</strong>: While stupid backoff does just as well with large language models than the most performant smoothing methods, its n-grams count is quite low compared to the billions of n-grams needed for the method to be accurate. Implementing a Kneser-Ney Smoothing or Katz Backoff instead, might improve the accuracy of the model, but would in turn make the application slower as these are more expensive.</li>
+                    <li><strong>Add more text to the corpus</strong>: As mentioned in the previous point, the n-gram count is quite low compared to state-of-the-art models. Adding more data to train the model will always yield better results.</li>
+                    <li><strong>Add sentence segmentation</strong>: I didn&apos;t use sentence tokenization methods as it is difficult to distinguish between a sentence-boundary marker and an abbreviation marker (e.g. Mr.). These require either an extensive dictionary of abbreviations or the use of machine learning. Instead, I segmented each sequences of words between each punctuation marker and built my model from there.</li>
+                    <li><strong>Implement an open vocabulary system</strong>: I didn&apos;t implement a system to deal with words that don&apos;t appear in my model. This would require either the use of machine learning or to build a fixed vocabulary set, then find out the probabilities that those words appear in a certain context.</li>
+                    <li><strong>Add more complexity</strong>: There is always more that we can add to a model to make it more accurate, but there is also a fine balance between accuracy and performance that we need to account for.</li>
+                </ul>
+             ")
     )
+  )
 )
+
 server <- function(input, output) {
-    word <- reactive({predict_word(input$string)})
+
+  words <- reactive({
+    if(input$string != ""){
+      predict_word(input$string, recursion = input$recursion)
+    } else{list(data.frame(Term="",Score=""))}
+  })
     
         output$prediction <- renderText({
-            paste("Suggested word:", word())
+            paste(str_trim(input$string), "<strong>", words()[[1]][1,1],"</strong>")
         })
-        output$text <- renderText({
-            paste(input$string, word())
-        })   
+        output$scores <- renderTable({head(words()[[1]],
+                                           min(input$slider,10))},
+                                     digits=4)
+
+        output$scores2 <- renderTable({
+          if(input$slider > 10 & words()[[2]] > 10){
+            words()[[1]][11:min(input$slider,20,words()[[2]]),]
+            }
+          }, digits=4)
+        
+        output$scores3 <- renderTable({
+          if(input$slider > 20 & words()[[2]] > 20){
+            words()[[1]][21:min(input$slider,30,words()[[2]]),]
+          }
+        }, digits=4)
+        output$table <- renderDT(
+            words()[[1]] %>%
+            datatable() %>%
+            formatRound(columns = "Score",digits=4))
 
 }
 shinyApp(ui, server)
--- App/functions.R
+++ App/functions.R
@@ -1,79 +1,75 @@
-## Main functions for predicting word
+## Script of functions for predicting word
 library(tidyverse)
 
-load("./ngrams.RData")
-bigrams <- dict[[1]]
-trigrams <- dict[[2]]
-fourgrams <- dict[[3]]
-fivegrams <- dict[[4]]
+# Loading dictionary of ngrams (dict)
+load("./ngrams.RData") 
+# It consists of a list of 5 tibbles
+  # [[1]] bigrams
+  # [[2]] trigrams
+  # [[3]] fougrams
+  # [[4]] fivegrams
+  # [[5]] unigrams (reduced to top 30)
 
 ## Clean String, return vector of 4 or less words
 clean_string <- function(string){
   string <- string %>%
     str_trim() %>%
     str_to_lower() %>%
-    str_replace_all("[^a-z' ]","") %>%
+    str_replace_all("['-]|['-]$|(?<![a-zA-Z])['-]|['-](?![a-zA-Z])","") %>%
+    str_replace_all("[^-a-z' ]","") %>%
+    str_replace_all("  +"," ") %>%
     str_split(" ")
   string <- string[[1]] %>%
     tail(4)
   return(string)
 }
 
-## Reduce string to existing ngram size
-find_ngram <- function(string, n){
-  ngram_found <- FALSE
+## Stupid BackOff
+## Receives a vector of words
+stupid_backoff <- function(words, alpha = 0.4, recursion = FALSE){ 
   
-  while(ngram_found == FALSE){
-    if (n==0){
-      message("word not found (find_ngram)")
-      string <- ""
+  n_words <- length(words)
+  n_alpha = 0
+  scores = NULL
+
+  ## Scoring
+  while(n_words>0){
+    words_str <- paste(words, collapse = " ")
+    ngrams <- dict[[n_words]] %>%
+      filter(firstTerms == words_str) %>%
+      mutate(Score = n/sum(n)*alpha^n_alpha) %>%
+      select(lastTerm, Score) %>%
+      rename(Term=lastTerm)
+    
+    scores <- bind_rows(scores, ngrams) %>%
+      arrange(desc(Score)) %>%
+      distinct(Term, .keep_all = TRUE)
+          
+    words <- words[-1]
+    n_words <- n_words-1
+    n_alpha <- n_alpha +1
+
+    if(nrow(scores) != 0 & recursion == FALSE) {
       break
-    }else if (string %in% dict[[n]][[1]]){
-      message("found ngram")
-      ngram_found <- TRUE
-      break
-    }else{
-      string <- strsplit(string," ")[[1]][-1] %>%
-        paste(collapse = " ")
-      n <- n-1
+    } else if(n_words==0 & recursion == FALSE){
+      scores <- dict[[5]] %>%
+        mutate(Score = Score*alpha^n_alpha)
+    } else if(n_words == 0 & nrow(scores) == 0){
+      unigrams <- dict[[5]] %>%
+        mutate(Score = Score*alpha^n_alpha)
+      scores <- scores %>%
+        bind_rows(unigrams) %>%
+        arrange(desc(Score)) %>%
+        distinct(Term, .keep_all = TRUE)
     }
   }
-  return(string)
-}
-
-## Get word
-get_words <- function(string){
-  n <- length(string)
-  string <- paste(string, collapse=" ")
-
-  string <- find_ngram(string, n)
-  n <- length(strsplit(string," ")[[1]])
   
-  if (n == 4){
-    word <- fivegrams %>%
-      filter(word1_4==string) %>%
-      select(word5)
-  } else if (n == 3){
-    word <- fourgrams %>%
-      filter(word1_3==string) %>%
-      select(word4)
-  } else if (n == 2){
-    word <- trigrams %>%
-      filter(word1_2==string) %>%
-      select(word3)
-  } else if (n == 1){
-    word <- bigrams %>%
-      filter(word1==string) %>%
-      select(word2)
-  } else {
-    word <- list("")
-  }
-  return(word[[1]])
+  return(scores)
 }
 
 ## main function
-predict_word <- function(input){
-  cleaned_input <- clean_string(input)
-  words <- get_words(cleaned_input)
-  return(words[1])
+predict_word <- function(inputString, recursion = FALSE){
+  cleaned_input <- clean_string(inputString)
+  scores <- stupid_backoff(cleaned_input, recursion=recursion)
+  list(scores, nrow(scores))
 }
